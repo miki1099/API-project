fcce0868d2d90d04cdc47d4d82d328ee0e41d0f9
Michal G <miki.glodek@wp.pl>
2020-12-12 10:28:28 +0100

    project core fore co-worker

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    }
+}
