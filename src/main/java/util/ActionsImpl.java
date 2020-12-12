4873851d96e53b3f0921932801053a6b380660cb
Michal G <miki.glodek@wp.pl>
2020-12-12 13:34:04 +0100

    added findYoungestCriminal method

--- src/main/java/util/ActionsImpl.java
+++ src/main/java/util/ActionsImpl.java
@@ -3,14 +3,17 @@ package util;
 import model.Criminal;
 import model.CriminalHolder;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ActionsImpl {
 
     private CriminalHolder criminalHolder;
 
-    private void createCriminalHoler() {
+    private void createCriminalHolder() {
         FBIStringReader fbiStringReader = new FBIStringReader();
         criminalHolder = fbiStringReader.getCriminalDetails();
     }
@@ -18,17 +21,18 @@ public class ActionsImpl {
 
 
     public void getRandomCriminal() {
-        createCriminalHoler();
+        createCriminalHolder();
         Random randomNumber = new Random();
         int rand = randomNumber.nextInt(criminalHolder.getItems().length);
         System.out.println(criminalHolder.getItems()[rand]);
     }
 
     public void getYoungestCriminal() {
-        createCriminalHoler();
-
-
-
+        createCriminalHolder();
+        List<Criminal> bufCriminalList = Arrays.asList(criminalHolder.getItems());
+        bufCriminalList = bufCriminalList.stream().filter(criminal -> !criminal.getAge_min().equals("None")).collect(Collectors.toList());
+        bufCriminalList.sort(Comparator.comparing(Criminal::getAge_min).reversed());
+        System.out.println(bufCriminalList.get(0));
     }
 
 }
