7e6d9a61d19a6c27166fe40a9b344105f4b701b3
Jacek <mrwp450@gmail.com>
2020-12-12 14:11:08 +0100

    merged getYoungestCriminal and getOldestCriminal in ActionsImpl

--- src/main/java/util/ActionsImpl.java
+++ src/main/java/util/ActionsImpl.java
@@ -5,13 +5,12 @@ import model.CriminalHolder;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ActionsImpl {
 
     private CriminalHolder criminalHolder;
 
-    private void createCriminalHoler() {
+    private void createCriminalHolder() {
         FBIStringReader fbiStringReader = new FBIStringReader();
         criminalHolder = fbiStringReader.getCriminalDetails();
     }
@@ -19,19 +18,21 @@ public class ActionsImpl {
 
 
     public void getRandomCriminal() {
-        createCriminalHoler();
+        createCriminalHolder();
         Random randomNumber = new Random();
         int rand = randomNumber.nextInt(criminalHolder.getItems().length);
         System.out.println(criminalHolder.getItems()[rand]);
     }
 
     public void getYoungestCriminal() {
-        createCriminalHoler();
-
+        createCriminalHolder();
+        List<Criminal> bufCriminalList = Arrays.asList(criminalHolder.getItems());
+        bufCriminalList = bufCriminalList.stream().filter(criminal -> !criminal.getAge_min().equals("None")).collect(Collectors.toList());
+        bufCriminalList.sort(Comparator.comparing(Criminal::getAge_min).reversed());
+        System.out.println(bufCriminalList.get(0));
     }
-
     public void getOldestCriminal() {
-        createCriminalHoler();
+        createCriminalHolder();
         List<Criminal> criminalList = Arrays.asList(criminalHolder.getItems());
         criminalList = criminalList.stream().filter(criminal -> !criminal.getAge_min().equals("None"))
                 .collect(Collectors.toList());
